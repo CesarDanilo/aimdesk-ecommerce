6b89810cc77ce863e347ed26459dffb3a7bde823
CSR <billypalacios616@gmail.com>
2025-07-04 12:49:53 -0300

    chore: refatorando migration [pedido]

--- backend/src/database/migrations/20240819193750-pedido.js
+++ backend/src/database/migrations/20240819193750-pedido.js
@@ -21,12 +21,25 @@ module.exports = {
         onDelete: 'SET NULL'
       },
       status: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false
+        type: Sequelize.STRING(30), // Ex: 'pendente', 'pago', 'enviado', 'cancelado'
+        allowNull: false,
+        defaultValue: 'pendente'
       },
       valorTotal: {
         type: Sequelize.FLOAT,
         allowNull: false
+      },
+      endereco_entrega: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      forma_pagamento: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      observacao: {
+        type: Sequelize.STRING(255),
+        allowNull: true
       },
       createdAt: {
         type: Sequelize.DATE
