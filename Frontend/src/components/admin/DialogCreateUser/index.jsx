82d5f90208dd5034e0c5f30e9d91d7f2fef1cd93
CSR <billypalacios616@gmail.com>
2025-07-04 14:46:39 -0300

    feat: criando campo senha e criando states

--- Frontend/src/components/admin/DialogCreateUser/index.jsx
+++ Frontend/src/components/admin/DialogCreateUser/index.jsx
@@ -1,6 +1,20 @@
 import { XMarkIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 
 export function DialogCreateUser({ onClose }) {
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [admin, setAdmin] = useState('false');
+    const [ativo, setAtivo] = useState('false');
+    const [cpf, setCpf] = useState('');
+    const [cidade, setCidade] = useState('');
+    const [endereco, setEndereco] = useState('');
+    const [bairro, setBairro] = useState('');
+    const [numero, setNumero] = useState('');
+    const [cep, setCep] = useState('');
+    const [referencia, setReferencia] = useState('');
+    const [contato, setContato] = useState('');
+    const [senha, setSenha] = useState('');
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
             <div className="w-full max-w-3xl bg-zinc-900 rounded-xl shadow-lg p-6 min-h-0 h-auto relative max-h-[85vh] overflow-y-auto scrollbar-minimal">
@@ -21,6 +35,8 @@ export function DialogCreateUser({ onClose }) {
                         <input
                             type="text"
                             id="nome"
+                            value={nome}
+                            onChange={e => setNome(e.target.value)}
                             className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                         />
                     </div>
@@ -30,6 +46,19 @@ export function DialogCreateUser({ onClose }) {
                         <input
                             type="email"
                             id="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block text-xs text-white mb-1" htmlFor="email">Senha</label>
+                        <input
+                            type="Senha"
+                            id="Senha"
+                            value={senha}
+                            onChange={e => setSenha(e.target.value)}
                             className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                         />
                     </div>
@@ -39,6 +68,8 @@ export function DialogCreateUser({ onClose }) {
                             <label className="block text-xs text-white mb-1" htmlFor="admin">Admin</label>
                             <select
                                 id="admin"
+                                value={admin}
+                                onChange={e => setAdmin(e.target.value)}
                                 className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                             >
                                 <option value="true">Sim</option>
@@ -50,6 +81,8 @@ export function DialogCreateUser({ onClose }) {
                             <label className="block text-xs text-white mb-1" htmlFor="ativo">Ativo</label>
                             <select
                                 id="ativo"
+                                value={ativo}
+                                onChange={e => setAtivo(e.target.value)}
                                 className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                             >
                                 <option value="true">Sim</option>
@@ -63,6 +96,8 @@ export function DialogCreateUser({ onClose }) {
                         <input
                             type="text"
                             id="cpf"
+                            value={cpf}
+                            onChange={e => setCpf(e.target.value)}
                             className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                         />
                     </div>
@@ -72,6 +107,8 @@ export function DialogCreateUser({ onClose }) {
                         <input
                             type="text"
                             id="cidade"
+                            value={cidade}
+                            onChange={e => setCidade(e.target.value)}
                             className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                         />
                     </div>
@@ -81,6 +118,8 @@ export function DialogCreateUser({ onClose }) {
                         <input
                             type="text"
                             id="endereco"
+                            value={endereco}
+                            onChange={e => setEndereco(e.target.value)}
                             className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                         />
                     </div>
@@ -90,6 +129,8 @@ export function DialogCreateUser({ onClose }) {
                         <input
                             type="text"
                             id="bairro"
+                            value={bairro}
+                            onChange={e => setBairro(e.target.value)}
                             className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                         />
                     </div>
@@ -100,6 +141,8 @@ export function DialogCreateUser({ onClose }) {
                             <input
                                 type="number"
                                 id="numero"
+                                value={numero}
+                                onChange={e => setNumero(e.target.value)}
                                 className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                             />
                         </div>
@@ -109,6 +152,8 @@ export function DialogCreateUser({ onClose }) {
                             <input
                                 type="text"
                                 id="cep"
+                                value={cep}
+                                onChange={e => setCep(e.target.value)}
                                 className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                             />
                         </div>
@@ -119,6 +164,8 @@ export function DialogCreateUser({ onClose }) {
                         <input
                             type="text"
                             id="referencia"
+                            value={referencia}
+                            onChange={e => setReferencia(e.target.value)}
                             className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                         />
                     </div>
@@ -128,6 +175,8 @@ export function DialogCreateUser({ onClose }) {
                         <input
                             type="text"
                             id="contato"
+                            value={contato}
+                            onChange={e => setContato(e.target.value)}
                             className="w-full rounded px-2 py-1 bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                         />
                     </div>
