7cb560aad16dbdbc848c43a5609f2e7dbaaa873f
CSR <billypalacios616@gmail.com>
2025-08-11 12:23:33 -0300

    feat: criando componente navbar-navgation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import { Button } from "./components/ui/button"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Homepage";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import { Navigation } from "../components/Navbar.component"
+
+function HomePage() {
+    return (
+        <div>
+            <Navigation />
+        </div>
+    )
+}
+
+export default HomePage
