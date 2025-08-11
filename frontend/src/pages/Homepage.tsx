ff7b9ff71a4929d7cd116946c0e7a2e1e2fb7059
CSR <billypalacios616@gmail.com>
2025-08-11 15:36:04 -0300

    chore: implementando componente navgation

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,9 +1,13 @@
 import { Navigation } from "../components/Navbar.component"
+import logo from '../assets/logo.png'
 
 function HomePage() {
     return (
         <div className="flex justify-center align-middle p-4">
-            <nav>
+            <nav className="flex gap-6">
+                <div className="flex w-40">
+                    <img src={logo} alt="logo" />
+                </div>
                 <Navigation />
             </nav>
         </div>
