ed023659aa431937b0e7089134c3605a7181b6aa
CSR <billypalacios616@gmail.com>
2025-08-12 12:01:54 -0300

    chore: regulando imagem

--- frontend/src/components/Section.component.tsx
+++ frontend/src/components/Section.component.tsx
@@ -1,10 +1,11 @@
 import { Card, CardContent } from "./ui/card"
 import banner from "../assets/banner.webp"
+
 export function SectionMainBanner() {
     return (
-        <Card className="flex w-10/12">
-            <CardContent>
-                <img src={banner} alt="" />
+        <Card className="flex w-10/12 border-none shadow-none bg-transparent">
+            <CardContent className="p-0">
+                <img src={banner} alt="Banner" className="w-full" />
             </CardContent>
         </Card>
     )
