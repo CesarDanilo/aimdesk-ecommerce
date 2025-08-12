d3bc7cf8ff056939516fef3bba7d0f983806ea9d
CSR <billypalacios616@gmail.com>
2025-08-12 16:45:53 -0300

    chore: regulando cards

--- frontend/src/components/CategoryCards.component.tsx
+++ frontend/src/components/CategoryCards.component.tsx
@@ -17,21 +17,21 @@ interface CategoryCardsProps {
 
 export function CategoryCards({ data }: CategoryCardsProps) {
     return (
-        <div className="grid grid-cols-3 gap-0.5 w-9/12 ">
+        <div className="grid grid-cols-3 gap-0.5 w-9/12">
             {data.map((item) => (
                 <Card
                     key={item.id}
-                    className="w-9/12 mx-auto border-none shadow-none bg-transparent relative overflow-hidden"
+                    className="w-9/12 mx-auto border-none rounded-2xl shadow-none bg-transparent relative overflow-hidden"
                 >
-                    <CardContent className="p-0">
+                    <CardContent className="p-0 rounded-2xl">
                         <img
                             src={item.image}
                             alt={item.name}
-                            className="w-full h-48 object-cover"
+                            className="w-full h-48 object-cover rounded-2xl"
                         />
                     </CardContent>
 
-                    <CardHeader className="absolute bottom-0 left-0 w-full p-3 bg-black bg-opacity-50">
+                    <CardHeader className="absolute bottom-0 left-0 w-full p-6 bg-black bg-opacity-50 rounded-b-2xl">
                         <CardTitle className="text-lg text-white">
                             {item.name}
                         </CardTitle>
