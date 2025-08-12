11aee33a5fec8fa436b5e193c5a7ba795c66c1ae
CSR <billypalacios616@gmail.com>
2025-08-12 17:05:33 -0300

    chore: regulando imagem

--- frontend/src/components/Section.component.tsx
+++ frontend/src/components/Section.component.tsx
@@ -1,9 +1,10 @@
 import { Card, CardContent } from "./ui/card"
+// import { Button } from "./ui/button"
 import banner from "../assets/banner.webm"
 
 export function SectionMainBanner() {
     return (
-        <Card className="flex w-10/12 border-none shadow-none bg-transparent ">
+        <Card className="relative flex w-10/12 border-none shadow-none bg-transparent rounded-2xl overflow-hidden">
             <CardContent className="p-0">
                 <video
                     className="w-full h-[500px] object-cover rounded-2xl"
@@ -14,6 +15,12 @@ export function SectionMainBanner() {
                     playsInline
                 />
             </CardContent>
-        </Card>
+
+            {/* <div className="absolute left-1/2 bottom-20 transform -translate-x-1/2">
+                <Button variant="default" className="bg-black text-white text-2xl shadow-lg hover:bg-gray-900">
+                    <h2>Saiba mais!</h2>
+                </Button>
+            </div> */}
+        </Card >
     )
 }
