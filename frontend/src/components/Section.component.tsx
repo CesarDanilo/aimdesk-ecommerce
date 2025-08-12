ee66877eea70747fc2f97b34943d6c010ca82616
CSR <billypalacios616@gmail.com>
2025-08-12 16:48:09 -0300

    chore: regulando cards

--- frontend/src/components/Section.component.tsx
+++ frontend/src/components/Section.component.tsx
@@ -3,10 +3,10 @@ import banner from "../assets/banner.webm"
 
 export function SectionMainBanner() {
     return (
-        <Card className="flex w-10/12 border-none shadow-none bg-transparent">
+        <Card className="flex w-10/12 border-none shadow-none bg-transparent ">
             <CardContent className="p-0">
                 <video
-                    className="w-full h-[500px] object-cover"
+                    className="w-full h-[500px] object-cover rounded-2xl"
                     src={banner}
                     autoPlay
                     loop
