1232997e6f9305637e29024ceceba2987798ce06
CSR <billypalacios616@gmail.com>
2025-08-13 10:00:17 -0300

    chore: update style cards catagory

--- frontend/src/components/CategoryCards.component.tsx
+++ frontend/src/components/CategoryCards.component.tsx
@@ -10,7 +10,7 @@ interface CategoryCardsProps {
 
 export function CategoryCards({ data }: CategoryCardsProps) {
     return (
-        <div className="grid grid-cols-3 gap-6 w-full max-w-6xl">
+        <div className="grid grid-cols-3 gap-6 w-9/12 max-w-6xl">
             {data.map((item) => (
                 <div
                     key={item.id}
@@ -22,7 +22,7 @@ export function CategoryCards({ data }: CategoryCardsProps) {
                         className="w-full h-full object-cover transform transition-transform duration-500 group-hover:scale-110"
                     />
                     <div className="absolute inset-0 bg-black/40 transition-colors duration-500 group-hover:bg-black/60"></div>
-                    <h3 className="absolute bottom-0 left-0 w-full p-3 text-base font-semibold text-white text-center transition-transform duration-500 group-hover:translate-y-[-4px]">
+                    <h3 className="absolute bottom-0 left-0 w-full p-3 text-2xl font-bold text-white text-center transition-transform duration-500 group-hover:translate-y-[-14px]">
                         {item.name}
                     </h3>
                 </div>
