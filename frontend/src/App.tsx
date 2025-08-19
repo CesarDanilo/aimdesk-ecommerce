783675b545877ca52aa9d9b1500160a8a9a870e6
CSR <billypalacios616@gmail.com>
2025-08-19 14:11:18 -0300

    chore: criando componente novo e redirecionando para checkout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
+import CheckoutCart from "./pages/CheckoutCart";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/checkout" element={<CheckoutCart />} />
       </Routes>
     </BrowserRouter>
   )
